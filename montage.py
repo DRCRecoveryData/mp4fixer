b1c4cd9c846ca68616a399965f5001327fbb0cfe
DRC Lab <85211068+DRCRecoveryData@users.noreply.github.com>
2025-02-20 01:33:53 +0700

    Update montage.py

--- montage.py
+++ montage.py
@@ -1,1 +1,17 @@
+import os
+import random
 
+block = 2000000  # size of block
+block_count = 35  # how many pieces you want
+
+# change file paths
+input_file = 'some_good_pron.mp4'
+output_file = 'montage2.bad'
+
+with open(input_file, 'rb') as dd, open(output_file, 'wb') as oo:
+    size = os.path.getsize(input_file)
+    
+    for _ in range(block_count):
+        dd.seek(random.randint(0, size - block))
+        file_data = dd.read(block)
+        oo.write(file_data)
